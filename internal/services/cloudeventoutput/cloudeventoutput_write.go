61bbeda93aab1666249293064475382420449006
herb stahl <ghstahl@gmail.com>
2023-11-01 09:03:40 -0700

    getting full kafka message

--- internal/services/cloudeventoutput/cloudeventoutput_write.go
+++ internal/services/cloudeventoutput/cloudeventoutput_write.go
@@ -2,6 +2,7 @@ package cloudeventoutput
 
 import (
 	"context"
+	"encoding/json"
 
 	benthos_service "github.com/benthosdev/benthos/v4/public/service"
 	proto_cloudeventprocessor "github.com/fluffy-bunny/benthos-cloudevents-fluffycore/pkg/proto/cloudeventprocessor"
@@ -16,6 +17,14 @@ func (s *service) Write(ctx context.Context, message *benthos_service.Message) e
 		log.Error().Err(err).Msg("failed to convert message to bytes")
 		return err
 	}
+	generic := []map[string]interface{}{}
+	err = json.Unmarshal(content, &generic)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to unmarshal generic")
+		return err
+	}
+	log.Info().Interface("generic", generic).Msg("generic")
+
 	ceBatch := &cloudevents.CloudEventBatch{}
 	err = protojson.Unmarshal(content, ceBatch)
 	if err != nil {
